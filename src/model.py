5c2604cbb53b5c773ef6c3fa1096565b5ea61558
Haseeb <11besemjaved@seecs.edu.pk>
2019-03-15 13:33:19 -0400

    Added pdfs decision trees

--- src/model.py
+++ src/model.py
@@ -2,11 +2,11 @@ from __future__ import print_function
 
 import os
 import subprocess
+import argparse
 
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-from sklearn.impute import SimpleImputer
-from sklearn.tree import DecisionTreeClassifier, export_graphviz
+from sklearn.tree import DecisionTreeClassifier, export_graphviz, DecisionTreeRegressor
 from sklearn.model_selection import train_test_split
 
 pd.set_option('display.max_columns', None)
@@ -43,6 +43,8 @@ def label_encode(df, target_column):
     targets = sorted(df_mod[target_column].unique())
     map_to_int = {name: n for n, name in enumerate(targets)}
     df_mod[target_column] = df_mod[target_column].replace(map_to_int)
+    print("Label encoding for: ", target_column)
+    print(map_to_int)
 
     return df_mod
 
@@ -71,35 +73,72 @@ def preprocess(raw_df):
 
     return df
 
-def visualize_tree(tree, feature_names):
-    """Create tree png using graphviz.
+def visualize_tree(tree, feature_names, step):
+    """Create tree png/pdf using graphviz.
 
     Args
     ----
     tree -- scikit-learn DecsisionTree.
     feature_names -- list of feature names.
     """
-    with open("dt1.dot", 'w') as f:
+    print("Generating DT graph")
+    name="dt-"+str(step)
+    dot_name=name+".dot"
+    png_name=name+".png"
+    pdf_name=name+".pdf"
+
+
+    with open(dot_name, 'w') as f:
         export_graphviz(tree, out_file=f,
                         feature_names=feature_names)
 
-    command = ["dot", "-Tpng", "dt1.dot", "-o", "dt.png"]
+    command1 = ["dot", "-Tpng", dot_name, "-o",  png_name]
+    command = ["dot", "-Tpdf", dot_name, "-o",  pdf_name]
     try:
         subprocess.check_call(command)
+        subprocess.check_call(command1)
     except:
-        exit("Could not run dot, ie graphviz, to "
+        exit("Could not run dot to "
              "produce visualization")
 
-if __name__ == '__main__':
-    raw_df=get_data()
+def decision_tree(split):
+    """
 
-    df=preprocess(raw_df)
+    :param split:
+    :return:
+    """
+    # Contains all columns except UserID, ProductID and Purchase
     features=list(df.columns[1:-1])
     X=df[features]
     y=df["Purchase"]
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1, test_size=0.33)
 
-
-    dt = DecisionTreeClassifier(min_samples_split=20, random_state=99)
+    print("Running DT with split:", split)
+    # dt = DecisionTreeClassifier(min_samples_split=split, random_state=99)
+    dt = DecisionTreeRegressor(min_samples_split=split, random_state=99)
     dt.fit(X_train, y_train)
-    visualize_tree(dt, features)
+    visualize_tree(dt, features, split)
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Run different ML models on the BlackFriday dataset'
+                                                 'If no argument specified then a simple linear regressor is run')
+    parser.add_argument('--dt', type=int,
+                        help='Run the decision tree model with DT minimum samples per splits and generate graph.'
+                             '~2500 would be okay')
+
+    args = parser.parse_args()
+
+    raw_df=get_data()
+    df=preprocess(raw_df)
+
+
+    if args.dt is not None:
+        decision_tree(args.dt)
+    else:
+        print("No other model implemented yet")
+
+
+
+
